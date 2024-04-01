3cc4bb6578663b963b2712883baffd23022fd39b
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2024-04-01 15:07:08 -0300

    3erDesafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,32 @@
+import express from "express";
 import productManager from "./servicios/productManager";
 
-const products = new productManager(path)
+const app = express();
+const port = 3000;
+
+const products = productManager
+
+app.get('/products', (req, res) => {
+    const { code } = req.query;
+    let { limit } = req.query;
+
+    if (!code || (code !== '18' && code !== '19')) {
+        return res.send("Código de producto no válido");
+    }
+
+    limit = parseInt(limit);
+    if (isNaN(limit) || limit <= 0) {
+        return res.send("El límite debe ser un número válido mayor que cero");
+    }
+
+    const filteredProducts = products.filter(product => product.code === code);
 
 
-    
+    const result = limit ? filteredProducts.slice(0, limit) : filteredProducts;
 
+    return res.json(result);
+});
+
+app.listen(port, () => {
+    console.log(`La aplicación está escuchando en el puerto ${port}`);
+});
