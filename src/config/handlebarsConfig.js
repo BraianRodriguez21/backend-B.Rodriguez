a12868ba7d690911fa63ec22dd48969466a2fa5d
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2024-04-24 08:15:26 -0300

    desafio websocket

--- src/config/handlebarsConfig.js
+++ src/config/handlebarsConfig.js
@@ -1,7 +1,5 @@
-// config/handlebarsConfig.js
 import engine from 'express-handlebars';
-//Configuracion para handlebars.
-//Esta configuracion permite usar archivo .hbs
+
 export const handlebarsConf = (app) => {
 	app.engine('hbs', engine({ extname: 'hbs' }));
 	app.set('views', 'src/views');
