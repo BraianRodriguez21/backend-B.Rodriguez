047eb82f7be2b2a5fd586f02e87b0a12ef0f500d
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2024-04-24 23:41:31 -0300

    desafio websocket 8

--- src/routes/viewRouter.js
+++ src/routes/viewRouter.js
@@ -25,12 +25,17 @@ export const viewRouter = Router();
 
 
 viewRouter.get('/', async (req, res) => {
-	
 	const response = await fetch('http://localhost:8080/api/products');
-
 	const data = await response.json();
-
 	res.render('home', {
-		books: data.payload,
+		products: data.payload,
 	});
+});
+
+viewRouter.get('/realtimeproducts', async (req, res) => {
+    const response = await fetch('http://localhost:8080/api/products/realtimeproducts');
+    const data = await response.json();
+    res.render('realtimeproducts', {
+        products: data.products, 
+    });
 });
