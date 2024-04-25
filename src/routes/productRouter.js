59d1a6f36f7dfe769abf12cd7499abeb3c587b31
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2024-04-24 23:24:52 -0300

    desafio websocket 8

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,34 +1,3 @@
-// import { Router } from 'express';
-// import { ProductManager } from '../servicios/productManager.js';
-// import { Product } from '../servicios/product.js';
-
-// export const productRouter = Router();
-
-// const productManager = new ProductManager();
-
-// productRouter.post('/', (req, res) => {
-//     try {
-//         const product = new Product(req.body);
-
-//         productManager.add(product);
-
-//         req.io.emit('new-product', product);
-
-//         res.json({ success: true, payload: product, message: 'Producto agregado' });
-//     } catch (error) {
-//         res.status(404).json({ success: false, payload: null, message: 'No se pudo agregar el producto' });
-//     }
-// });
-// productRouter.get('/', (req, res) => {
-//     try {
-//         const products = productManager.getProducts();
-//         res.json({ success: true, payload: products, message: 'Productos obtenidos' });
-//     } catch (error) {
-//         res.json({ success: false, payload: null, message: 'No se pudo obtener los productos' });
-//     }
-// });
-// src/routes/productRouter.js
-
 import { Router } from 'express';
 import { ProductManager } from '../servicios/productManager.js';
 import { Product } from '../servicios/product.js';
@@ -40,23 +9,23 @@ const productManager = new ProductManager();
 productRouter.post('/', (req, res) => {
     try {
         const product = new Product(req.body);
+
         productManager.add(product);
+
         req.io.emit('new-product', product);
         res.json({ success: true, payload: product, message: 'Producto agregado' });
     } catch (error) {
         res.status(404).json({ success: false, payload: null, message: 'No se pudo agregar el producto' });
     }
 });
-
 productRouter.get('/', (req, res) => {
     try {
         const products = productManager.getProducts();
         res.json({ success: true, payload: products, message: 'Productos obtenidos' });
     } catch (error) {
-        res.status(500).json({ success: false, payload: null, message: 'Error al obtener los productos' });
+        res.json({ success: false, payload: null, message: 'No se pudo obtener los productos' });
     }
 });
-
 productRouter.get('/realtimeproducts', (req, res) => {
     try {
         const products = productManager.getProducts();
@@ -65,3 +34,6 @@ productRouter.get('/realtimeproducts', (req, res) => {
         res.status(500).json({ success: false, payload: null, message: 'Error al renderizar la vista de productos en tiempo real' });
     }
 });
+
+
+
